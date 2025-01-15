cf9339b5c437d62a0c384eb1596de7e69089844d
Jacobchestnut16 <88856888+Jacobchestnut16@users.noreply.github.com>
2025-01-15 14:54:51 -0500

    Day 2

---
+++ src/sql_setup/create_tables.php
@@ -0,0 +1,51 @@
+<?php
+
+$servername = "database"; //will change after new env setup
+$username = "user";
+$password = "pass";
+$dbname = "mcshedual";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+}
+
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname;
+
+        CREATE TABLE IF NOT EXISTS users (
+        id INT NOT NULL AUTO_INCREMENT,
+        username VARCHAR(100) NOT NULL,
+        password VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        phone VARCHAR(15) NOT NULL),
+        PRIMARY KEY (id);
+        
+        CREATE TABLE IF NOT EXISTS EventLocation (
+        id INT NOT NULL AUTO_INCREMENT,
+        location varchar(100) NOT NULL),
+        PRIMARY KEY (id);
+        
+        CREATE TABLE IF NOT EXISTS EventType (
+        id INT NOT NULL AUTO_INCREMENT,
+        eventType varchar(100) NOT NULL),
+        PRIMARY KEY (id);
+        
+        CREATE TABLE IF NOT EXISTS events (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(100) NOT NULL,
+        date DATETIME NOT NULL,
+        eventTypeID int NOT NULL,
+        locationID int NOT NULL),
+        PRIMARY KEY (id),
+        FOREIGN KEY (eventTypeID) REFERENCES EventType (id),
+        FOREIGN KEY (locationID) REFERENCES EventLocation (id));;
+        
+        CREATE TABLE IF NOT EXISTS EventUsers (
+        id INT NOT NULL AUTO_INCREMENT,
+        eventID INT NOT NULL,
+        userID INT NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (eventID) REFERENCES events (id),
+        FOREIGN KEY (userID) REFERENCES users (id));
+        ";
