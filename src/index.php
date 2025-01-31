10d18d17739a6a0d8f34758d4403a44563f8340c
Jacobchestnut16 <88856888+Jacobchestnut16@users.noreply.github.com>
2025-01-31 15:26:09 -0500

    Day 8,9

--- src/index.php
+++ src/index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Monteverde Clocks</title>
+    <link rel="stylesheet" href="static/style/dropdown.css">
+</head>
+<body>
 <?php
 
 $servername = "database"; //will change after new env setup
@@ -19,38 +27,99 @@ if (isset($_GET['navWindow'])) {
 <table>
     <tr>
         <?php if (isset($_GET['uc'])):?>
-        <td>
             <?php
 
 
-            $sql = "SELECT * FROM `users` where id = ".$_GET['uc'].";";
-            $result = $conn->query($sql);
-            $row = $result->fetch_assoc();
-            $UserPrivilege = $row['privilege'];
+//            $sql = "SELECT * FROM `users` where id = ".$_GET['uc'].";";
+//            $result = $conn->query($sql);
+//            $row = $result->fetch_assoc();
+//            $UserPrivilege = $row['privilege'];
+//            $extendedView = $row['extendedView']; //update the db for admin
+            $extendedView = 1;
+            $UserPrivilege = 1;
 
-//            will make this all a switch case soon so that im not repeating so much code
-            if ($UserPrivilege < 2):
+
+            switch (true) {
+                case ($UserPrivilege == 2):
+                    echo "<td><form><button type='submit'>Extend View</button></form></td>";
+                case ($UserPrivilege == 1 || $UserPrivilege == 2):
+                    echo "
+                                <td>
+                                     <div class='dropdown-container'>
+                                          <a>Crew</a>
+                                          <div class='dropdown'>
+                                              <table>
+                                                  <tr><td><a href='/?navWindow=nav/admin/addWorker.html&uc=".$_GET['uc']."' title='Add new worker profile'>Add Worker</a></td></tr>
+                                                  <tr><td><a href='/?navWindow=nav/Calendar.html&uc=" . $_GET['uc'] . "' title='View, Edit, Remove crew'>Worker Profile</a></td></tr>
+                                              </table>
+                                          </div>
+                                      </div>
+                                </td>
+                                <td><div class='dropdown-container'>
+                                    <a>Event</a>
+                                    <div class='dropdown'>
+                                        <table>
+                                            <tr><td><a href='' title='Create an event on the calendar'>Create Event</a></td></tr>
+                                            <tr><td><a href='/?navWindow=nav/admin/Event.php&uc=".$_GET['uc']."' title='Edit the list of known events'>Events List Editor</a></td></tr>
+                                            <tr><td><a href='/?navWindow=nav/admin/Location.php&uc=".$_GET['uc']."' title='Edit the list of know locations'>Locations List Editor</a></td></tr>
+                                        </table>
+                                    </div>
+                                </div></td>
+                        ";
+                case $extendedView >= 0:
+                    echo "
+                      <td>
+                          <div class='dropdown-container'>
+                              <a>Time Clock</a>
+                              <div class='dropdown'>
+                                  <table>
+                                  <tr><td><a href='/?navWindow=nav/TimeClock.html&uc=".$_GET['uc']."'>Time Clock</a></td></tr>
+                                  <tr><td><a href='/?navWindow=nav/Calendar.html&uc=" . $_GET['uc'] . "'>Missed punch form</a></td></tr>
+                                  </table>
+                              </div>
+                          </div>
+                      </td>
+                      <td>
+                          <div class='dropdown-container'>
+                              <a>Calendar</a>
+                              <div class='dropdown'>
+                                  <table>
+                                  <tr><td><a href='/?navWindow=nav/Schedule.html&uc=" . $_GET['uc'] . "'>Schedule</a></td></tr>
+                                  <tr><td><a href='/?navWindow=nav/Calendar.html&uc=" . $_GET['uc'] . "'>Calendar</a></td></tr>
+                                  <tr><td><a href='/?navWindow=nav/RequestOff.html&uc=" . $_GET['uc'] . "'>Request Off</a></td></tr>
+                                  </table>
+                              </div>
+                          </div>
+                      </td>
+                      <td>
+                          <div class='dropdown-container'>
+                              <a>Schedule</a>
+                              <div class='dropdown'>
+                                  <table>
+                                      <tr><td><a href='/?navWindow=nav/Schedule.html&uc=" . $_GET['uc'] . "'>Schedule</a></td></tr>
+                                      <tr><td><a href='/?navWindow=nav/Calendar.html&uc=" . $_GET['uc'] . "'>Request Days</a></td></tr>
+                                      <tr><td><a href='/?navWindow=nav/RequestOff.html&uc=" . $_GET['uc'] . "'>Request Off</a></td></tr>
+                                  </table>
+                              </div>
+                          </div>
+                      </td>
+                      <td><a href='/?navWindow=nav/Email.html&uc=" . $_GET['uc'] . "'>Email</a> <!--This may not stay a feature, or may become owner only--></td>
+                      <td>
+                         <div class='dropdown-container'>
+                              <a>Profile</a>
+                              <div class='dropdown'>
+                                  <table>
+                                      <tr><td><a href='/?navWindow=nav/Schedule.html&uc=" . $_GET['uc'] . "'>Profile</a></td></tr>
+                                      <tr><td><a href='/?navWindow=nav/Calendar.html&uc=" . $_GET['uc'] . "'>Reset Passord</a></td></tr>
+                                      <tr><td><a href='/?navWindow=nav/RequestOff.html&uc=" . $_GET['uc'] . "'>Logout</a></td></tr>
+                                  </table>
+                              </div>
+                          </div>
+                      </td>
+                    ";
+            }
+
             ?>
-            <a href="/?navWindow=addWorker.html">Time Clock</a>
-            <a href="/?navWindow=addWorker.html">Schedule</a>
-            <a href="/?navWindow=addWorker.html">Calendar</a>
-            <a href="/?navWindow=addWorker.html">Request Off</a>
-            <a href="/?navWindow=addWorker.html">Email</a> <!--This may not stay a feature, or may become owner only-->
-                <?php if ($UserPrivilege == 1): ?>
-                <a href="/?navWindow=addWorker.html">Add Worker</a>
-                <?php endif;?>
-
-            <?php elseif ($UserPrivilege == 2): ?>
-                <a href="/?navWindow=addWorker.html">Time Clock</a>
-                <a href="/?navWindow=addWorker.html">Schedule</a>
-                <a href="/?navWindow=addWorker.html">Calendar</a>
-                <a href="/?navWindow=addWorker.html">Request Off</a>
-                <a href="/?navWindow=addWorker.html">Email</a> <!--This may not stay a feature, or may become owner only-->
-                <?php if ($row['extendedView'] == 1): ?>
-                    <a href="/?navWindow=addWorker.html">Add Worker</a>
-                <?php endif;?>
-            <?php endif;?>
-        </td>
         <?php else: ?>
         <td>
             <a href="/?navWindow=prereq/register.html">Sign up</a>
@@ -61,3 +130,5 @@ if (isset($_GET['navWindow'])) {
 </table>
 
 <iframe src="<?= $navWindow ?>" frameborder="1" width="100%" height="90%"></iframe>
+</body>
+</html>
---
+++ src/nav/admin/Event.php
@@ -0,0 +1,7 @@
+<form action="../../action/addEvent.php">
+    <label for="event">Event: </label>
+    <input type="text" id="event" name="event" required>
+    <button type="submit">Add</button>
+</form>
+
+<?php
